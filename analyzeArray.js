784b9c96978e6f6aa259115924d0bafe68a40a9d
Manon <manonlefkos@gmail.com>
2023-06-23 00:11:05 +0200

    write tests for analyzeArray.js

---
+++ analyzeArray.js
@@ -0,0 +1,9 @@
+export default function analyzeArray(array) {
+  const object = {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  }
+  return object
+}
---
+++ analyzeArray.test.js
@@ -0,0 +1,34 @@
+import analyzeArray from "./analyzeArray";
+
+test("function doesn't return an array", () => {
+  expect(Array.isArray(analyzeArray([]))).toBeFalsy();
+});
+
+test("function returns object", () => {
+  expect(typeof analyzeArray([])).toBe("object");
+});
+
+test("function returns average. [1, 8, 3, 4, 2, 6] average should return 4", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test("function returns min. [1, 8, 3, 4, 2, 6] min should return 1", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test("function returns max. [1, 8, 3, 4, 2, 6] max should return 8", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test("function should return length. [1, 8, 3, 4, 2, 6] length should return 6", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
+});
+
+test("function returns all object keys correctly", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
